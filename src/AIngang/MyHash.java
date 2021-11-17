fdc34eb024cbc6113bcde582ea8738c83fedcff0
younggwon1 <dlrjsapdlf622@naver.com>
2021-11-17 20:27:45 +0900

    해쉬 테이블  (Hash Table)

---
+++ src/AIngang/MyHash.java
@@ -0,0 +1,91 @@
+package AIngang;
+
+public class MyHash {
+    public Slot[] hashTable;
+
+    public MyHash(Integer size) {
+        this.hashTable = new Slot[size];
+    }
+
+    public class Slot {
+        String key;
+        String value;
+        Slot(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public int hashFunc(String key) {
+        return (int)(key.charAt(0)) % this.hashTable.length;
+    }
+
+    public boolean saveData(String key, String value) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            if (this.hashTable[address].key == key) {
+                this.hashTable[address].value = value;
+                return true;
+            } else {
+                Integer currAddress = address + 1;
+                while (this.hashTable[currAddress] != null) {
+                    if (this.hashTable[currAddress].key == key) {
+                        this.hashTable[currAddress].value = value;
+                        return true;
+                    } else {
+                        currAddress++;
+                        if (currAddress >= this.hashTable.length) {
+                            return false;
+                        }
+                    }
+                }
+                this.hashTable[currAddress] = new Slot(key, value);
+                return true;
+            }
+        } else {
+            this.hashTable[address] = new Slot(key, value);
+        }
+        return true;
+    }
+
+    public String getData(String key) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            if (this.hashTable[address].key == key) {
+                return this.hashTable[address].value;
+            } else {
+                // 참고: 다음 코드를 수정합니다.
+                // Integer currAddress = address + 1;
+                // 예외 케이스로, 키에 해당하는 주소가 가장 마지막 슬롯일 경우,
+                // this.hashTable[address + 1] 에 해당하는 배열은 없기 때문에,
+                // 예외 케이스에서도 동작하도록 currAddress 는 address 만 대입하고 진행합니다
+                Integer currAddress = address; // 수정
+                while (this.hashTable[currAddress] != null) {
+                    if (this.hashTable[currAddress].key == key) {
+                        return this.hashTable[currAddress].value;
+                    } else {
+                        currAddress++;
+                        if (currAddress >= this.hashTable.length) {
+                            return null;
+                        }
+                    }
+                }
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyHash mainObject = new MyHash(20);
+        mainObject.saveData("DaveLee", "01011112222");
+        mainObject.saveData("fun-coding", "01022223333");
+        mainObject.saveData("David", "01033334444");
+        mainObject.saveData("Dave", "01044445555");
+
+        System.out.println(mainObject.getData("DaveLee"));
+        System.out.println(mainObject.getData("David"));
+        System.out.println(mainObject.getData("Dave"));
+    }
+}
