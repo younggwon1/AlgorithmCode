d9cda37aebd16e02390b850e737b1688a1a0b05e
younggwon1 <dlrjsapdlf622@naver.com>
2021-12-22 20:53:56 +0900

    그래프 탐색 코드

---
+++ src/AIngang/BFSSearch.java
@@ -0,0 +1,40 @@
+package AIngang;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Arrays;
+
+public class BFSSearch {
+    public ArrayList<String> bfsFunc(HashMap<String, ArrayList<String>> graph, String startNode) {
+        ArrayList<String> visited = new ArrayList<String>();
+        ArrayList<String> needVisit = new ArrayList<String>();
+
+        needVisit.add(startNode);
+
+        while (needVisit.size() > 0) {
+            String node = needVisit.remove(0);
+            if (!visited.contains(node)) {
+                visited.add(node);
+                needVisit.addAll(graph.get(node));
+            }
+        }
+        return visited;
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, ArrayList<String>> graph = new HashMap<String, ArrayList<String>>();
+        graph.put("A", new ArrayList<String>(Arrays.asList("B", "C")));
+        graph.put("B", new ArrayList<String>(Arrays.asList("A", "D")));
+        graph.put("C", new ArrayList<String>(Arrays.asList("A", "G", "H", "I")));
+        graph.put("D", new ArrayList<String>(Arrays.asList("B", "E", "F")));
+        graph.put("E", new ArrayList<String>(Arrays.asList("D")));
+        graph.put("F", new ArrayList<String>(Arrays.asList("D")));
+        graph.put("G", new ArrayList<String>(Arrays.asList("C")));
+        graph.put("H", new ArrayList<String>(Arrays.asList("C")));
+        graph.put("I", new ArrayList<String>(Arrays.asList("C", "J")));
+        graph.put("J", new ArrayList<String>(Arrays.asList("I")));
+
+        BFSSearch bObject = new BFSSearch();
+        System.out.println(bObject.bfsFunc(graph, "A"));
+    }
+}
